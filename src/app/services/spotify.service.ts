17a5cba2e8a28cca16aa6359a3d99b0b19760f72
Daniel Cunha <danielbrandaodacunha@gmail.com>
2017-05-04 17:14:42 -0300

    albums added

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -6,6 +6,8 @@ import 'rxjs/add/operator/map';
 export class SpotifyService {
   private searchUrl : string;
   private artistUrl : string;
+  private albumsUrl : string;
+  
   constructor(private _http : Http) { 
         
   }
@@ -21,6 +23,12 @@ export class SpotifyService {
     return this._http.get(this.artistUrl)
       .map(res => res.json());
 
+  }
+
+  getAlbums(artistId : string){
+    this.albumsUrl = 'https://api.spotify.com/v1/artists/'+ artistId + '/albums';
+    return this._http.get(this.albumsUrl)
+      .map(res => res.json());
   }
 
 }
